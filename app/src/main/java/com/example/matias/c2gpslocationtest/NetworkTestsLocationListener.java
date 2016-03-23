b39b9bfd20e72d38bdf9626988f9e6f70d4e5c07
Matias Pyy <matias.pyy@gmail.com>
2016-03-23 20:36:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/matias/c2gpslocationtest/NetworkTestsLocationListener.java
@@ -0,0 +1,30 @@
+package com.example.matias.c2gpslocationtest;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+/**
+ * Created by matias on 19.3.2016.
+ */
+public class NetworkTestsLocationListener implements LocationListener {
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
